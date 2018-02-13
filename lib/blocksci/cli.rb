3fd846e151e56f56142b7b579c9c00fb0e2a1bc5
Jun Okumura <okumura@haw.co.jp>
2018-02-13 11:57:46 +0900

    feat: Add max_block_height and some tx_counts temporary.

--- lib/blocksci/cli.rb
+++ lib/blocksci/cli.rb
@@ -12,6 +12,17 @@ module BlockSci
       chain_blocks = parser.update_chain
       data_configuration = BlockSci::Util::DataConfiguration.new(File.expand_path(options[:output_directory]))
       starting_tx_count = get_starting_tx_count(data_configuration)
+      max_block_height = chain_blocks.newest_block.height
+
+      total_tx_count = 0
+      total_input_count = 0
+      total_output_count = 0
+      chain_blocks.block_list.each do |(k, block)|
+        total_tx_count += block.tx_count
+        total_input_count += block.input_count
+        total_output_count += block.output_count
+      end
+      
     end
 
     private
